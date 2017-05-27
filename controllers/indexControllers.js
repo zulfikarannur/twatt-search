c02461a068c284e7c454816a709733567193c100
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-05-27 15:24:36 +0700

    neat view

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -6,7 +6,7 @@ const testing = function(req,res) {
 
 const searchReturn = function(req,res) {
   twitter.twitOauth.get(
-    `https://api.twitter.com/1.1/search/tweets.json?q=%40${req.params.keyword}`,
+    `https://api.twitter.com/1.1/search/tweets.json?q=%40${req.params.keyword}&count=10`,
     process.env.TEST_USER_TOKEN, //test user token
     process.env.TEST_USER_SECRET, //test user secret
       function (e, data, result){
@@ -15,7 +15,16 @@ const searchReturn = function(req,res) {
         }
         else if (data){
           console.log("Data");
-          res.send(JSON.parse(data).statuses)
+          var status_many = []
+          JSON.parse(data).statuses.forEach(function(status) {
+            var status_one = {}
+            status_one['text'] = `${status.text}`
+            status_one['user_id'] = `${status.user.id}`
+            status_one['user_name'] = `${status.user.name}`
+            status_many.push(status_one)
+          })
+          res.send(status_many)
+          // res.send(JSON.parse(data).statuses)
         }
         else if(result){
           console.log("Result");
